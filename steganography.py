51e8f8ebd34d98460d0de7f5898f3956a32b4190
nathanieltan <nathaniel98.tan@gmail.com>
2017-03-10 14:41:59 -0500

    Finished toolbox

--- steganography.py
+++ steganography.py
@@ -9,6 +9,7 @@ def decode_image(file_location="images/encoded_sample.png"):
     """
     encoded_image = Image.open(file_location)
     red_channel = encoded_image.split()[0]
+    redPixels = red_channel.load()
 
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
@@ -16,7 +17,18 @@ def decode_image(file_location="images/encoded_sample.png"):
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
 
-    pass #TODO: Fill in decoding functionality
+
+    for x in range(x_size):
+        for y in range(y_size):
+            pixel = redPixels[x,y]
+
+            binaryString = bin(pixel)
+
+            if binaryString[-1] == '1':
+                pixels[x,y] = (255,255,255)
+            else:
+                pixels[x,y] = (0,0,0)
+
 
     decoded_image.save("images/decoded_image.png")
 
@@ -43,11 +55,32 @@ def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
     text_to_encode: the text to encode into the template image
     template_image: the image to use for encoding. An image is provided by default.
     """
-    pass #TODO: Fill out this function
+    unencoded_image = Image.open(template_image)
+    unencodedPixels = unencoded_image.load()
+
+    textImage = write_text(text_to_encode, unencoded_image.size)
+    encoded_image = Image.new("RGB", unencoded_image.size)
+
+    textPixels = textImage.load()
+    pixels = encoded_image.load()
+
+    redPixels = (unencoded_image.split()[0]).load()
+    greenPixels = (unencoded_image.split()[1]).load()
+    bluePixels = (unencoded_image.split()[2]).load()
+
+    for x in range(unencoded_image.size[0]):
+        for y in range(unencoded_image.size[1]):
+            binaryString = bin(redPixels[x,y])
+            if textPixels[x,y] == (255,255,255):
+                binaryString = binaryString[0:-1]+'1'
+            else:
+                binaryString = binaryString[0:-1]+'0'
+            pixels[x,y] = (int(binaryString,2),greenPixels[x,y],bluePixels[x,y])
+    encoded_image.save("images/encoded_image.png")
 
 if __name__ == '__main__':
     print("Decoding the image...")
     decode_image()
 
     print("Encoding the image...")
-    encode_image()
+    encode_image("wow I like memes, and wow, Sophie Li is like, the best Soft Des ninja ever. I was like, totally not paid off to say that. But like, I could be paid to say that in the future. <3")
